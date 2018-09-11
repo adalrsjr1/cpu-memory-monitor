7585dfa216b1a9e3ddb87a4f2831de4f0f03ffbc
Adalberto Junior <adalrsjr1@gmail.com>
2018-09-11 19:02:18 +0000

    code refactored

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 
-## Saving PID of a current process with Java 8 or before
+## Self-saving PID in Java 8 or before
 
-`
+```
 int savePid(String filepath) {
 		int pid = Integer.parseInt(new File("/proc/self").getCanonicalFile().getName())
 		File tmp = new File(filepath)
-		tmp.write("$pid")
+		tmp.write(pid)
 		return pid
-	}
-`
+}
+```
--- monitor.py
+++ monitor.py
@@ -9,44 +9,88 @@ import time
 from collections import deque
 import multiprocessing
 
-fig = plt.figure()
-ax = plt.axes()
-ax.set_ylim(0,100*multiprocessing.cpu_count())
-ax.set_xlim(0,200)
-ax2 = ax.twinx()
-ax2.set_ylim(0,psutil.virtual_memory().total / 1024/1024/1024)
-ax2.set_xlim(0,200)
-cpu_line, = ax.plot([],[], color="red")
-mem_line, = ax2.plot([],[], color="blue")
-
-cpu_y_list = deque([-1]*400)
-mem_y_list = deque([-1]*400)
-x_list = deque(np.linspace(200,0,num=400))
-
-process = None
-
 def init():
     cpu_line.set_data([],[])
     mem_line.set_data([],[])
     return cpu_line, mem_line
 
+def toKilo(size_bytes):
+    return size_bytes / 1024
+
+def toMega(size_bytes):
+    return toKilo(size_bytes) / 1024
+
+def toGiga(size_bytes):
+    return toMega(size_bytes) / 1024
+
+def change_base(notation, size_bytes):
+    """
+        convert memory in bytes to:
+            kilo == k or K
+            mega == m or M
+            giga == g or G
+    """
+    if "k" == notation or "K" == notation:
+        return toKilo(size_bytes)
+    if "m" == notation or "M" == notation:
+        return toMega(size_bytes)
+    if "g" == notation or "G" == notation:
+        return toGiga(size_bytes)
+    return size_bytes
 
 def animate(i):
     cpu_y_list.pop()
     mem_y_list.pop()
-    cpu_y_list.appendleft(process.cpu_percent())
-    print("{} {} ".format(process.cpu_percent(.01), process.memory_percent() * psutil.virtual_memory().total/100/1024/1024/1024) )
-    mem_y_list.appendleft(process.memory_percent() * psutil.virtual_memory().total / 100/1024/1024/1024)
+
+    cpu_percent = process.cpu_percent(.01)
+    mem_percent = process.memory_percent()
+    total_mem = psutil.virtual_memory().total
+
+    mem_used = change_base(memory_base, mem_percent * total_mem) / 100
+
+    cpu_y_list.appendleft(cpu_percent)
+    mem_y_list.appendleft(mem_used)
+
+    print("{} {} ".format(cpu_percent, mem_used) )
+
     cpu_line.set_data(x_list,cpu_y_list)
     mem_line.set_data(x_list,mem_y_list)
+
     return cpu_line, mem_line
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("\nusage: python monitor.py <pid_file>\n")
+    if len(sys.argv) != 3:
+        print("\nusage: python monitor.py <memory_notation B K M G> <pid_file>\n")
         sys.exit(1)
 
-    pid_filename = sys.argv[1]
+    memory_base = sys.argv[1]
+    pid_filename = sys.argv[2]
+
+    fig = plt.figure()
+
+    ax = plt.axes()
+    ax.set_ylim(0,100 * multiprocessing.cpu_count())
+    ax.set_xlim(0,60)
+    ax.set_xlabel('last 60 seconds')
+    ax.set_ylabel('CPU (%)', color='red')
+
+    ax2 = ax.twinx() # copy of plot area
+    ax2.set_ylim(0,change_base(memory_base, psutil.virtual_memory().total)/1000)
+    ax2.set_xlim(0,60)
+
+    ax2.set_ylabel('Memory ({}B)'.format('' if memory_base == 'b' or memory_base == 'B' else memory_base), color='blue')
+
+    cpu_line, = ax.plot([],[], color="red")
+    mem_line, = ax2.plot([],[], color="blue")
+
+    time_window = 450
+
+    cpu_y_list = deque([-1]*time_window)
+    mem_y_list = deque([-1]*time_window)
+    x_list = deque(np.linspace(0,60,num=time_window)) # dynamic x-axis
+
+    process = None
+
     while not os.path.exists(pid_filename):
         time.sleep(.1) # wait 100 milliseconds
 
@@ -56,6 +100,5 @@ if __name__ == '__main__':
 
         anim = animation.FuncAnimation(fig, animate, init_func=init,
             frames=200, interval=100, blit=True)
-
 
     plt.show()
